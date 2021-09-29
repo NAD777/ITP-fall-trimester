b50ad186f4b36aeb8c559e5711818fff1962b3bf
Anton Nehaev <30959736+NAD777@users.noreply.github.com>
2021-09-29 12:12:08 +0300

    Update c.java

--- Lab_29_09_21/ex1/c.java
+++ Lab_29_09_21/ex1/c.java
@@ -7,9 +7,15 @@ public class Main {
 
     public static void main(String[] args) {
         // write your code here
-        for (int i = 0; i < 26; i++) {
-            System.out.format("Index %d - %c - %c\n", i + 1, 'a' + i, 'A' + i);
+        Scanner scanner = new Scanner(System.in);
+        char c = scanner.next().charAt(0);
+        if ('a' <= c && c <= 'z') {
+            System.out.print(c - 'a' + 1);
+        } else if ('A' <= c && c <= 'Z') {
+            System.out.print(c - 'A' + 1);
+        } else {
+            System.out.print("Invalid input!");
         }
-
+        scanner.close();
     }
 }
