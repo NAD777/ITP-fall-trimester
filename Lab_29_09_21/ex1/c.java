197258490d3946ae27bca3ec854754053645d3eb
Anton Nehaev <30959736+NAD777@users.noreply.github.com>
2021-09-29 11:06:05 +0300

    Add solve for ex1 b

---
+++ Lab_29_09_21/ex1/c.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        Scanner scanner = new Scanner(System.in);
+        char c = scanner.next().charAt(0);
+        if ('a' <= c && c <= 'z') {
+            System.out.print(c - 'a' + 1);
+        } else if ('A' <= c && c <= 'Z') {
+            System.out.print(c - 'A' + 1);
+        } else {
+            System.out.print("Invalid input!");
+        }
+        scanner.close();
+    }
+}
