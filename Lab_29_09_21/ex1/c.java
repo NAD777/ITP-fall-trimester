749f56ab8363928e53cf90acc28f259e23ae4d3c
Anton Nehaev <30959736+NAD777@users.noreply.github.com>
2021-09-29 12:01:34 +0300

    Update solution of ex1 c

--- Lab_29_09_21/ex1/c.java
+++ Lab_29_09_21/ex1/c.java
@@ -1,3 +1,4 @@
+
 package com.company;
 
 import java.util.Scanner;
@@ -7,7 +8,7 @@ public class Main {
     public static void main(String[] args) {
         // write your code here
         for (int i = 0; i < 26; i++) {
-            System.out.format("Index %d - %c\n", i + 1, 'a' + i);
+            System.out.format("Index %d - %c - %c\n", i + 1, 'a' + i, 'A' + i);
         }
 
     }
